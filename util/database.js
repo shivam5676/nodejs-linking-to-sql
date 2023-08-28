526b45ec2e4e622ca37a204c5cc8c672ea10f80c
shivam5676 <ssultanpura9@gmail.com>
2023-08-28 08:46:58 -0700

    sequelise introduced to maintain sql database complexities

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
-const mysql=require("mysql2")
-const pool=mysql.createPool({
-    host:"localhost",
-    user:'root',
-    database:'node-complete',
-    password:'(@Shivam'
-})
+const Sequelize = require("sequelize");
 
-module.exports=pool.promise();
+const sequelize = new Sequelize("node-complete", "root", "(@Shivam", {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports=sequelize
